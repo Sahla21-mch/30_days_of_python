9f68a743349c2b73773901e3a487adcc01dfb06d
Sahla21-mch <you@example.com>
2022-11-17 02:39:15 +0100

    intro to classes

---
+++ day1.py
@@ -0,0 +1,16 @@
+#intro to python classes
+class Employee:
+
+    def __init__(self,firstN,secondN,salary,specification):
+        self.firstN = firstN
+        self.secondN = secondN
+        self.salary = salary
+        self.specification = specification
+
+    def fullName(self):
+        return "{} {}".format(self.firstN, self.secondN)
+
+emp_1 = Employee('Sandrine','Nyuyki', 1200000, 'mananger')
+print(Employee.fullName(emp_1))
+
+
