6d56934cf4488029a4420eecfc0c5c15d4418fa9
Sahla21-mch <you@example.com>
2022-11-23 21:42:41 +0100

    corrections

--- day5.py
+++ day5.py
@@ -1,10 +1,9 @@
 class Person:
-      def __init__(self, fname, lname):
+    def __init__(self, fname, lname):
         self.firstname = fname
         self.lastname = lname
-
-        def printname(self):
-            print(self.firstname, self.lastname)
+    def printname(self):
+        print(self.firstname, self.lastname)
 
 #Use the Person class to create an object, and then execute the printname method:
 
